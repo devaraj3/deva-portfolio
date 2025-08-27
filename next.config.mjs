0f4c5fe735ff901c0860cad5afc891c51f3fcec4
Devaraj P <97376562+devaraj3@users.noreply.github.com>
2025-08-27 17:20:57 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,3 @@
-import mdx from '@next/mdx'
-
-const withMDX = mdx()
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    mdxRs: true,
-  },
-}
-
-export default withMDX(nextConfig)
+const nextConfig = {};
+export default nextConfig;
